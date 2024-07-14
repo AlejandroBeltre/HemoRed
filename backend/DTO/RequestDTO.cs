b86d7bcfdada2dbcb2ef5262043d77752fcf37f8
EqualNoches <119954229+EqualNoches@users.noreply.github.com>
2024-07-14 10:57:43 -0400

    Fixed bug request model.
    Didn't retrieve foreign key when scaffolding was made.

--- backend/DTO/RequestDTO.cs
+++ backend/DTO/RequestDTO.cs
@@ -18,7 +18,7 @@ public class NewRequestDto
 {
     public string UserDocument { get; set; }
     public int BloodTypeId { get; set; }
-    public int BloodBank { get; set; }
+    public int BloodBankId { get; set; }
     public DateTime RequestTimeStamp { get; set; }
     public string RequestReason { get; set; }
     public int RequestedAmount { get; set; }
