bf52ae20c50ceec6a65004d395fd0cbf6666034f
Alejandro Beltre <1115242@est.intec.edu.do>
2024-07-14 07:09:55 -0400

    fixed problem mergin

--- Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
+++ Backend.Tests/obj/Debug/net8.0/Backend.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e5d36c98dd90ebee085fbe2b9271efb866adaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f094f4a3f70a62b13cb8dbaa7354ff159e47a12")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
