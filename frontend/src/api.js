1ae3143bb0a75ff279cc3dadb26776377c107b14
Alejandro <alejandro.beltre134@gmail.com>
2025-04-15 00:24:39 -0400

    added configurations

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5178/api',
+    baseURL: 'http://hemored-production.up.railway.app//api',
 });
 
 api.interceptors.request.use(
