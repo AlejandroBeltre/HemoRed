981ed6e0b333e10c373b829a31345a1b365c4a7d
Alejandro <alejandro.beltre134@gmail.com>
2025-04-20 01:24:07 -0400

    updated to https requests

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://hemored-production.up.railway.app//api',
+    baseURL: 'https://hemored-production.up.railway.app//api',
 });
 
 api.interceptors.request.use(
