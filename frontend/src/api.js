3a020c9801f69bc0c2c71625a4dcd1898d98a2fd
Alejandro <alejandro.beltre134@gmail.com>
2025-04-20 01:24:28 -0400

    updated url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://hemored-production.up.railway.app//api',
+    baseURL: 'https://hemored-production.up.railway.app/api',
 });
 
 api.interceptors.request.use(
